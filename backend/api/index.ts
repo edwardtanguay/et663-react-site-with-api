842351dffddb2c98a217e370a776c2e74888832b
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-03 11:37:59 +0100

    getting data from backend

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as config from '../config';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const port = 4882;
 
 app.get('/', (req, res) => {
+	res.status(203);
 	res.json({
 		appName: config.getAppName() 
 	});
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const backendUrl = "http://localhost:4882";
+
 export const PageWelcome = () => {
+	const [appMessage, setAppMessage] = useState(""); 
+	const [appName, setAppName] = useState("");
+
+	const fetchAppName = async () => {
+		try {
+			const response = await axios.get(backendUrl);
+			console.log('STATUS: ' + response.status);
+			setAppName(response.data.appName);
+		} catch (e: any) {
+			console.log(`ERROR: ${e.message}`);
+			setAppMessage(
+				`Sorry, we can't retrieve your data at this time. Try again later.`
+			);
+		}
+	};
+
+	useEffect(() => {
+		fetchAppName();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2 className="text-red-600">{appMessage}</h2>
+			{appMessage === "" && (
+				<>
+					<p>APP NAME: {appName}</p>
+				</>
+			)}
+		</>
+	);
+};
