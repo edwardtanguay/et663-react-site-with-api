488a0f50d2bfb7953651c2006d5befb4277b3616
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-03 13:10:52 +0100

    finished

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -4,15 +4,22 @@ import axios from "axios";
 
 const backendUrl = "http://localhost:4882";
 
+interface IEmpHireItem {
+	fullName: string;
+	hireDate: string;
+}
+
 export const PageWelcome = () => {
-	const [appMessage, setAppMessage] = useState(""); 
+	const [appMessage, setAppMessage] = useState("");
 	const [appName, setAppName] = useState("");
-	const [nodeVersion, setNodeVersion] = useState('');
+	const [nodeVersion, setNodeVersion] = useState("");
+	const [numberOfFiles, setNumberOfFiles] = useState(0);
+	const [empHireItems, setEmpHireItems] = useState<IEmpHireItem[]>([]);
 
 	const fetchAppName = async () => {
 		try {
 			const response = await axios.get(backendUrl);
-			console.log('STATUS: ' + response.status);
+			console.log("STATUS: " + response.status);
 			setAppName(response.data.appName);
 		} catch (e: any) {
 			console.log(`ERROR: ${e.message}`);
@@ -31,7 +38,23 @@ export const PageWelcome = () => {
 			const response = await axios.get(`${backendUrl}/node-version`);
 			setNodeVersion(response.data);
 		})();
-	},[])
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}/number-of-files`);
+			setNumberOfFiles(response.data);
+		})();
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(
+				`${backendUrl}/employee-hire-dates`
+			);
+			setEmpHireItems(response.data);
+		})();
+	}, []);
 
 	return (
 		<>
@@ -40,6 +63,20 @@ export const PageWelcome = () => {
 				<>
 					<p>APP NAME: {appName}</p>
 					<p>NODE-VERSION: {nodeVersion}</p>
+					<p>NUMBER OF FILES: {numberOfFiles}</p>
+					<p>
+						EMPLOYEE HIRE DATES:
+						<ul className="list-disc ml-6">
+							{empHireItems.map((empHireItem, index) => {
+								return (
+									<li key={index}>
+										{empHireItem.fullName} -{" "}
+										{empHireItem.hireDate}
+									</li>
+								);
+							})}
+						</ul>
+					</p>
 				</>
 			)}
 		</>
